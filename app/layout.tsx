b618cc9b033a99e5ecaaf8dc76191a0429bd8c56
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-06-16 07:01:28 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,15 @@
+import { Providers } from './providers'
 
-"use client";
-
-import { ChakraProvider } from '@chakra-ui/react';
-import Spinner from "@/spinner";
-import {Suspense} from "react";
-
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
       <body>
-        
-        <ChakraProvider>
-<Suspense fallback={null}>
-          <Spinner />
-
-          {children}
-</Suspense>
-        </ChakraProvider>
-          
+        <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
